e152575fcc23851519c426f39f19196c4a95d23e
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-05-23 14:50:09 +0600

    User get, post patch and delete api added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
@@ -10,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.zxai2xc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxai2xc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,9 +25,88 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
 
+        const userCollection = client.db("bistroDb").collection("users");
+        const menuCollection = client.db("bistroDb").collection("menu");
+        const reviewsCollection = client.db("bistroDb").collection("reviews");
+        const cartsCollection = client.db("bistroDb").collection("carts");
+
+
+        // user collection 
+        app.get('/users', async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+
+            // insert email if user doesn't exists:
+            const query = {email: user?.email};
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: 'User already exists', insertedId: null})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result)
+
+        })
+
+        app.delete('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+         // menu collection 
+        app.get('/menu', async(req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
+
+          // reviews collection 
+        app.get('/reviews', async(req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
+        // carts collection 
+        app.get('/carts', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email}
+            const result = await cartsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/carts', async(req, res) => {
+            const cartItem = req.body;
+            const result = await cartsCollection.insertOne(cartItem);
+            res.send(cartItem);
+        })
+
+        app.delete('/carts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartsCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
